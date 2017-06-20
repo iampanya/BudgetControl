d7855a4b5f7536196c3d3bed8937f8f9f3abe6d4
504798 <504798@C2ITSIS03>
2017-06-20 11:44:17 +0700

    BudgetManager V.1

--- BudgetControl/Manager/BudgetManager.cs
+++ BudgetControl/Manager/BudgetManager.cs
@@ -60,22 +60,73 @@ namespace BudgetControl.Manager
             budgetRepo.Save();
         }
 
-        public void Delete(Budget budget)
-        {
+        //public void Delete(Budget budget)
+        //{
            
-        }
+        //}
 
         #endregion
 
 
         #region Refactor
-        private GenericRepository<Budget> budgetRepo = new GenericRepository<Budget>();
 
-        #region Get Budget
+        private GenericRepository<Budget> _budgetRepo = new GenericRepository<Budget>();
+
+        #region Create
+
+        // Change to Add() later
+        public void AddBudget(Budget budget)
+        {
+            // 1. Validate entity
+            this.Validate(budget);
+
+            // 2. Set new timestamp
+            budget.NewCreateTimeStamp();
+
+            // 3. Add budget to database
+            _budgetRepo.Add(budget);
+            _budgetRepo.Save();
+        }
+
+        public void AddByForm(CreateBudgetModel form)
+        {
+            // 1. Check AccountID is exist in database?
+            AccountManager accManager = new AccountManager();
+            if (accManager.GetByID(form.AccountID) == null)
+            {
+                accManager.Add(new Account()
+                {
+                    AccountID = form.AccountID,
+                    AccountName = form.AccountName,
+                    Status = RecordStatus.Active
+                });
+            }
+
+            // 2. Check Budget is exist in database?
+            if(GetBudget(form.Year, form.AccountID, form.CostCenterID) == null)
+            {
+                this.AddBudget(new Budget()
+                {
+                    BudgetID = Guid.NewGuid(),
+                    AccountID = form.AccountID,
+                    CostCenterID = form.CostCenterID,
+                    Sequence = 0,
+                    Year = form.Year,
+                    BudgetAmount = form.Amount,
+                    WithdrawAmount = 0,
+                    RemainAmount = 0,
+                    Status = BudgetStatus.Active
+                });
+            }
+        }
+
+        #endregion
+
+        #region Read
 
         public IEnumerable<Budget> GetAll()
         {
-            return budgetRepo.Get();
+            return _budgetRepo.Get();
         }
 
         private IEnumerable<Budget> GetByStatus(BudgetStatus status)
@@ -108,54 +159,90 @@ namespace BudgetControl.Manager
                   ).FirstOrDefault();
         }
 
+        public Budget GetByID(Guid budgetid)
+        {
+            return _budgetRepo.GetById(budgetid);
+        }
+
 
         #endregion
 
+        #region Update
 
-        #region Add Budget
-        
-        public void AddBudget(Budget budget)
+        // Change to Update() later
+        public void UpdateBudget(Budget budget)
         {
-            // Set new timestamp
-            budget.NewCreateTimeStamp();
+            // 1. Validate entity
+            this.Validate(budget);
 
-            // Add budget to database
-            budgetRepo.Add(budget);
-            budgetRepo.Save();
+            // 2. Modify timestamp
+            budget.NewModifyTimeStamp();
+
+            //3. Update to database
+            _budgetRepo.Update(budget);
+            _budgetRepo.Save();
         }
 
-        public void AddBudgetByForm(CreateBudgetModel form)
-        {
-            // 1. Check AccountID is exist in database?
-            AccountManager accManager = new AccountManager();
-            if (accManager.GetByID(form.AccountID) == null)
-            {
-                accManager.Add(new Account()
-                {
-                    AccountID = form.AccountID,
-                    AccountName = form.AccountName,
-                    Status = RecordStatus.Active
-                });
-            }
+        #endregion
 
-            // 2. Check Budget is exist in database?
-            if(GetBudget(form.Year, form.AccountID, form.CostCenterID) == null)
-            {
-                this.AddBudget(new Budget()
-                {
-                    BudgetID = Guid.NewGuid(),
-                    AccountID = form.AccountID,
-                    CostCenterID = form.CostCenterID,
-                    Sequence = 0,
-                    Year = form.Year,
-                    BudgetAmount = form.Amount,
-                    WithdrawAmount = 0,
-                    RemainAmount = 0,
-                    Status = BudgetStatus.Active
-                });
-            }
+        #region Deactivate
+
+        public void Deactivate(Guid budgetid)
+        {
+            this.Deactivate(this.GetByID(budgetid));
         }
 
+        public void Deactivate(Budget budget)
+        {
+            // 1. Validate entity
+            this.Validate(budget);
+
+            // 2. Change budget status to Cancelled
+            budget.Status = BudgetStatus.Cancelled;
+            this.Update(budget);
+        }
+
+        #endregion
+
+        #region Delete
+
+        public void Delete(Guid budgetid)
+        {
+            this.Delete(this.GetByID(budgetid));
+        }
+
+        public void Delete(Budget budget)
+        {
+            //1. Validate entity
+            this.Validate(budget);
+
+            // 2. Modify timestamp
+            budget.NewModifyTimeStamp();
+
+            // 3. Delete from datatbase
+            _budgetRepo.Delete(budget);
+            _budgetRepo.Save();
+        }
+
+        #endregion
+
+        #region Validate
+
+        public void Validate(Budget budget)
+        {
+            // 1. Check entity is null
+            if (budget == null)
+            {
+                throw new Exception("Budget cannot be null");
+            }
+
+            // 2. Check content inside entity
+            var validateResult = budget.Validate();
+            if (!validateResult.IsSuccess)
+            {
+                throw new Exception(validateResult.ErrorMessage);
+            }
+        }
         #endregion
 
         #endregion
