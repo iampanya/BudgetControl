0d459eacbcfb3f980c4f75e5165fdcc3948c0880
504798 <504798@C2ITSIS03>
2017-06-20 10:09:36 +0700

    Add custom validation result class

---
+++ BudgetControl/Models/Base/ValidationResult.cs
@@ -0,0 +1,22 @@
+﻿namespace BudgetControl.Models.Base
+{
+    public class ValidationResult
+    {
+        public ValidationResult(bool isSuccess)
+        {
+            this.IsSuccess = isSuccess;
+        }
+
+        public ValidationResult(string errorMessage, string memberName)
+        {
+            this.IsSuccess = false;
+            this.ErrorMessage = errorMessage;
+            this.MemberName = memberName;
+        }
+
+        public bool IsSuccess { get; set; }
+        public string ErrorMessage { get; set; }
+        public string MemberName { get; set; }
+
+    }
+}
