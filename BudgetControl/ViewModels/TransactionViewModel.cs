8e27f3e4dcd6a1fa1d9c45f35b0a4f667f02590d
504798 <504798@C2ITSIS03>
2017-06-29 15:55:46 +0700

    Add new TransactionViewModel file

---
+++ BudgetControl/ViewModels/TransactionViewModel.cs
@@ -0,0 +1,38 @@
+﻿using BudgetControl.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BudgetControl.ViewModels
+{
+    public class TransactionViewModel
+    {
+
+        #region Constructor
+
+        public TransactionViewModel()
+        {
+
+        }
+
+        #endregion
+
+        #region Fields
+
+        public string BudgetTransactionID { get; set; }
+        public string BudgetID { get; set; }
+        public string PaymentID { get; set; }
+
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public decimal PreviousAmount { get; set; }
+        public decimal RemainAmount { get; set; }
+        public string Status { get; set; }
+
+        public RecordTimeStamp Timestamp { get; set; }
+
+        #endregion
+
+    }
+}
