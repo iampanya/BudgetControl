63e22d04571a2f960c20259edb403a849d43489a
504798 <504798@C2ITSIS03>
2017-07-07 15:40:08 +0700

    Add authorize attribute

---
+++ BudgetControl/Attributes/ZAuthorizeAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BudgetControl.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class ZAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            //switch (Roles)
+            //{
+
+            //}
+            var isAuthorized = base.AuthorizeCore(httpContext);
+
+            return isAuthorized;
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+
+            //base.HandleUnauthorizedRequest(filterContext);
+            filterContext.Result = new RedirectToRouteResult(
+                new RouteValueDictionary(
+                    new
+                    {
+                        controller = "Users",
+                        action = "Unauthorized"
+                    })
+                );
+        }
+    }
+}
