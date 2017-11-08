08fbea21ad35d1205350b82d572c697464f8ead0
504798 <panya.cs13@gmail.com>
2017-11-08 13:58:40 +0700

    payment index : add year list from 2559 to current + 1 and set default to current year

--- BudgetControl/js/payment.controller.js
+++ BudgetControl/js/payment.controller.js
@@ -34,11 +34,18 @@
         function callSuccess(response) {
             vm.payments = hr.respondSuccess(response);
             
-            vm.years = $filter('unique')(vm.payments, 'Year') || [{ Year: '2560' }];
-            if (vm.years.length < 1) {
-                vm.years.push({ Year: new Date().getFullYear() + 543 + '' });
+            //vm.years = $filter('unique')(vm.payments, 'Year') || [{ Year: '2560' }];
+            //if (vm.years.length < 1) {
+            //    vm.years.push({ Year: new Date().getFullYear() + 543 + '' });
+            //}
+            //vm.year = vm.years[0].Year;
+
+            vm.years = [];
+            var currentYear = new Date().getFullYear();
+            for (var i = 2016; i <= currentYear + 1; i++) {
+                vm.years.push({ Year: i + 543 + '' });
             }
-            vm.year = vm.years[0].Year;
+            vm.year = currentYear + 543 + '';
         }
 
         //// 1.2 Call to server fail.
