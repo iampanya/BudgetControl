569ef8ce16cbe62a8975e21612928779f030cd63
504798 <panya.cs13@gmail.com>
2017-11-20 14:38:38 +0700

    add sql for update data after migration

--- BudgetControl/Migrations/201711130812557_Add payment type and owner cca.cs
+++ BudgetControl/Migrations/201711130812557_Add payment type and owner cca.cs
@@ -9,6 +9,10 @@ namespace BudgetControl.Migrations
         {
             AddColumn("dbo.Payment", "OwnerCostCenterID", c => c.String());
             AddColumn("dbo.Payment", "Type", c => c.Int(nullable: false));
+
+            Sql("UPDATE Payment SET [Type] = 1 WHERE CostCenterID NOT LIKE 'Z%'");
+            Sql("UPDATE Payment SET [Type] = 2 WHERE CostCenterID LIKE 'Z%'");
+            Sql("UPDATE Payment SET [OwnerCostCenterID] = [CostCenterID] WHERE CostCenterID NOT LIKE 'Z%'");
         }
         
         public override void Down()
