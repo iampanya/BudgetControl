7206bfd343cb5cdf0c8c8327e4a78edb114acdc7
504798 <panya.cs13@gmail.com>
2017-12-20 16:32:11 +0700

    Add contractor manager

---
+++ BudgetControl/Manager/ContractorManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BudgetControl.Models;
+
+namespace BudgetControl.Manager
+{
+    public class ContractorManager
+    {
+        //private BudgetContext
+
+        #region Constructor
+
+        public ContractorManager()
+        {
+
+        }
+
+        #endregion
+
+        #region Create
+
+        public Contractor Add(Contractor entity)
+        {
+            return null;
+        }
+
+        #endregion
+
+        #region Read
+
+        public IQueryable<Contractor> Get()
+        {
+            return null;
+        }
+
+        #endregion
+    }
+}
