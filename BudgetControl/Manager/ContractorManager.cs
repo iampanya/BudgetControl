4de396cf1b3bbbea62faebc765d576faab890414
504798 <panya.cs13@gmail.com>
2017-12-20 16:47:26 +0700

    Add method for adding contractor

--- BudgetControl/Manager/ContractorManager.cs
+++ BudgetControl/Manager/ContractorManager.cs
@@ -1,29 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using BudgetControl.DAL;
 using BudgetControl.Models;
+using BudgetControl.Models.Base;
 
 namespace BudgetControl.Manager
 {
     public class ContractorManager
     {
-        //private BudgetContext
+        private BudgetContext _db;
 
         #region Constructor
 
         public ContractorManager()
         {
+            _db = new BudgetContext();
+        }
 
+        public ContractorManager(BudgetContext context)
+        {
+            _db = context;
         }
 
         #endregion
 
         #region Create
 
-        public Contractor Add(Contractor entity)
+        public Contractor Add(string costcenterid, string name)
         {
-            return null;
+            Contractor contractor = new Contractor();
+            contractor.ID = Guid.NewGuid();
+            contractor.Name = name;
+            contractor.Status = RecordStatus.Active;
+            contractor.NewCreateTimeStamp();
+            _db.Entry(contractor).State = EntityState.Added;
+            return contractor;
         }
 
         #endregion
@@ -33,6 +47,20 @@ namespace BudgetControl.Manager
         public IQueryable<Contractor> Get()
         {
             return null;
+        }
+        
+        public IQueryable<Contractor> GetByCostCenterID()
+        {
+            return null;
+        }
+
+        #endregion
+
+        #region Save
+
+        public void Save()
+        {
+            _db.SaveChanges();
         }
 
         #endregion
