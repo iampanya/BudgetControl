e18c2ebdc6aadb0f45f54159d413b8db345bff22
504798 <panya.cs13@gmail.com>
2017-12-22 13:57:18 +0700

    Add employee manager

---
+++ BudgetControl/Manager/EmployeeManager.cs
@@ -0,0 +1,77 @@
+﻿using BudgetControl.DAL;
+using BudgetControl.IdmEmployeeServices;
+using BudgetControl.Models;
+using BudgetControl.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BudgetControl.Manager
+{
+    public class EmployeeManager
+    {
+        private BudgetContext _db;
+
+        #region Constructor
+        
+        public EmployeeManager()
+        {
+            _db = new BudgetContext();
+        }
+
+        public EmployeeManager(BudgetContext context)
+        {
+            _db = context;
+        }
+
+        #endregion
+
+
+        #region Update
+
+        public Employee UpdateFromIDM(EmployeeProfile empProfile)
+        {
+            Employee employee;
+            string employeeid = empProfile.EmployeeId.TrimStart(new char[] { '0' });
+
+            // 1. Get employee from database
+            var employeeInDb = _db.Employees
+                .Where(e => e.EmployeeID == employeeid && e.Status == RecordStatus.Active)
+                .FirstOrDefault();
+
+            // 2. Convert from EmployeeProfile to Employee
+            employee = new Employee(empProfile);
+
+            if (employeeInDb == null)
+            {
+                // Add new
+                employee.NewCreateTimeStamp();
+                _db.Entry(employee).State = System.Data.Entity.EntityState.Added;
+                _db.SaveChanges();
+            }
+            else
+            {
+                // Check has change
+                if (employeeInDb.HasChange(employee))
+                {
+                    //Update
+                    employeeInDb.FirstName = employee.FirstName;
+                    employeeInDb.LastName = employee.LastName;
+                    employeeInDb.JobTitle = employee.JobTitle;
+                    employeeInDb.JobLevel = employee.JobLevel;
+                    employeeInDb.CostCenterID = employee.CostCenterID;
+                    employeeInDb.Status = RecordStatus.Active;
+                    employeeInDb.NewModifyTimeStamp();
+                    _db.Entry(employeeInDb).State = System.Data.Entity.EntityState.Modified;
+                    _db.SaveChanges();
+                }
+            }
+
+            return employee;
+
+        }
+
+        #endregion
+    }
+}
