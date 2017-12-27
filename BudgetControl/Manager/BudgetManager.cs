3305f4120d524257d716119afdf5e817229e7e8c
iampanya <panya.cs13@gmail.com>
2017-12-27 22:59:09 +0700

    Add method to get budget with transaction detail

--- BudgetControl/Manager/BudgetManager.cs
+++ BudgetControl/Manager/BudgetManager.cs
@@ -16,6 +16,10 @@ namespace BudgetControl.Manager
         private BudgetContext _db;
         private string conn_string = ConfigurationManager.ConnectionStrings["BudgetContext"].ConnectionString;
 
+        #region SQL Command
+
+        #region CMD: Budget Summary
+
         private string cmd_budget_summary =
             @"
                 SELECT b.BudgetID, b.AccountID, a.AccountName, b.CostCenterID
@@ -36,6 +40,52 @@ namespace BudgetControl.Manager
 		                AND b.Status = 1
                 ORDER BY b.AccountID		
             ";
+
+        #endregion
+
+        #region CMD: Budget Info
+
+        private string cmd_budget_info =
+            @"
+                SELECT TOP(1) b.BudgetID, b.AccountID, a.AccountName, b.CostCenterID, c.CostCenterName, b.Year, b.BudgetAmount, b.Status
+                FROM	Budget b
+		                INNER JOIN Account a ON (b.AccountID = a.AccountID)
+		                INNER JOIN CostCenter c ON (b.CostCenterID = c.CostCenterID) 
+                WHERE	BudgetID = @BudgetID
+		                AND b.Status = 1
+            ";
+
+        #endregion
+
+        #region CMD: Budget with transaction
+
+        private string cmd_budget_transaction =
+            @"
+                SELECT  bt1.BudgetTransactionID, p.Description, bt1.Amount, bt2.PreviousAmount
+		                , bt1.CreatedAt, bt1.CreatedBy, bt1.ModifiedAt, bt1.ModifiedBy
+		                , p.PaymentID, p.PaymentNo, p.Type, p.RequestBy, p.ContractorID
+                FROM	BudgetTransaction bt1
+		                INNER JOIN Payment p ON (bt1.PaymentID = p.PaymentID)
+		                INNER JOIN (
+			                SELECT	t1.BudgetTransactionID, (SUM(t2.Amount) - t1.Amount) As PreviousAmount
+			                FROM	BudgetTransaction t1 
+					                LEFT OUTER JOIN BudgetTransaction t2 ON (t1.CreatedAt >= t2.CreatedAt)
+					                INNER JOIN Payment p ON (t1.PaymentID = p.PaymentID)
+			                WHERE	t1.BudgetID = @BudgetID 
+					                AND t2.BudgetID = @BudgetID
+					                AND t2.Status = 1
+					                AND p.Status = 1
+			                GROUP BY t1.BudgetTransactionID, t1.Amount
+		                ) AS bt2 ON (bt1.BudgetTransactionID = bt2.BudgetTransactionID)
+                WHERE bt1.BudgetID = @BudgetID
+		                AND bt1.Status = 1
+		                AND p.Status = 1
+                ORDER BY bt1.CreatedAt
+            ";
+
+        #endregion
+
+        #endregion
         #region Constructor
 
         public BudgetManager()
@@ -92,9 +142,86 @@ namespace BudgetControl.Manager
 
         #region Get budget with transaction
 
-        public void GetWithTransaction(string id)
+        public Budget GetWithTransaction(string id)
         {
+            Budget budget;
 
+            using (SqlConnection conn = new SqlConnection(conn_string))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand(cmd_budget_info, conn))
+                {
+                    cmd.Parameters.AddWithValue("BudgetID", id);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            budget = new Budget();
+                            budget.BudgetID = Guid.Parse(id);
+                            budget.AccountID = reader["AccountID"].ToString();
+                            budget.Account = new Account()
+                            {
+                                AccountID = budget.AccountID,
+                                AccountName = reader["AccountName"].ToString()
+                            };
+                            budget.CostCenterID = reader["CostCenterID"].ToString();
+                            budget.CostCenter = new CostCenter()
+                            {
+                                CostCenterID = budget.CostCenterID,
+                                CostCenterName = reader["CostCenterName"].ToString()
+                            };
+                            budget.Year = reader["Year"].ToString();
+                            budget.BudgetAmount = decimal.Parse(reader["BudgetAmount"].ToString());
+                            budget.Status = (BudgetStatus)int.Parse(reader["Status"].ToString());
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+
+                using (SqlCommand cmd = new SqlCommand(cmd_budget_transaction, conn))
+                {
+                    cmd.Parameters.AddWithValue("BudgetID", id);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        budget.BudgetTransactions = new List<BudgetTransaction>();
+                        budget.WithdrawAmount = 0;
+                        while (reader.Read())
+                        {
+                            BudgetTransaction bt = new BudgetTransaction();
+                            bt.BudgetTransactionID = Guid.Parse(reader["BudgetTransactionID"].ToString());
+                            bt.Description = reader["Description"].ToString();
+                            bt.Amount = decimal.Parse(reader["Amount"].ToString());
+                            bt.PreviousAmount = decimal.Parse(reader["PreviousAmount"].ToString());
+                            bt.CreatedAt = DateTime.Parse(reader["CreatedAt"].ToString());
+                            bt.CreatedBy = reader["CreatedBy"].ToString();
+                            bt.ModifiedAt = DateTime.Parse(reader["ModifiedAt"].ToString());
+                            bt.ModifiedBy = reader["ModifiedBy"].ToString();
+                            bt.PaymentID = Guid.Parse(reader["PaymentID"].ToString());
+                            bt.Payment = new Payment()
+                            {
+                                PaymentNo = reader["PaymentNo"].ToString(),
+                                Type = (PaymentType)int.Parse(reader["Type"].ToString()),
+                                RequestBy = reader["RequestBy"].ToString(),
+                                Description = reader["Description"].ToString(),
+                                ContractorID = reader.IsDBNull(reader.GetOrdinal("ContractorID")) ? (Guid?)null : Guid.Parse(reader["ContractorID"].ToString())
+                            };
+                            bt.RemainAmount = budget.BudgetAmount - bt.Amount - bt.PreviousAmount;
+                            budget.BudgetTransactions.Add(bt);
+                            budget.WithdrawAmount += bt.Amount;
+                        }
+
+                    }
+                }
+                conn.Close();
+            }
+
+            budget.RemainAmount = budget.BudgetAmount - budget.WithdrawAmount;
+
+            return budget;
         }
 
         #endregion
@@ -160,7 +287,7 @@ namespace BudgetControl.Manager
 
         public void Delete(Budget budget)
         {
-           
+
         }
 
 
@@ -176,7 +303,7 @@ namespace BudgetControl.Manager
         public IEnumerable<BudgetViewModel> ConvertToVMs(List<Budget> budgets)
         {
             List<BudgetViewModel> vms = new List<BudgetViewModel>();
-            foreach(var budget in budgets)
+            foreach (var budget in budgets)
             {
                 vms.Add(ConvertToVM(budget));
             }
