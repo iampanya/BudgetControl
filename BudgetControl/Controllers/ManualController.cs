63745ec38e2aa22569477ecfd786ad768196a370
iampanya <panya.cs13@gmail.com>
2017-12-28 22:29:21 +0700

    Add manual controller

---
+++ BudgetControl/Controllers/ManualController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BudgetControl.Controllers
+{
+    public class ManualController : Controller
+    {
+        // GET: Manual
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
