7c225bb10653c8a35d14f8608df7ce57aa547a80
504798 <panya.cs13@gmail.com>
2018-01-04 09:17:09 +0700

    refactor migration class

--- BudgetControl/Migrations/Configuration.cs
+++ BudgetControl/Migrations/Configuration.cs
@@ -12,13 +12,12 @@ namespace BudgetControl.Migrations
 
     internal sealed class Configuration : DbMigrationsConfiguration<BudgetControl.DAL.BudgetContext>
     {
-        List<Employee> employees = new List<Employee>();
         List<User> users = new List<User>();
-        List<Account> accounts = new List<Account>();
-        List<CostCenter> costcenters = new List<CostCenter>();
-        List<Budget> budgets = new List<Budget>();
         List<BudgetTransaction> transactions = new List<BudgetTransaction>();
         List<Payment> payments = new List<Payment>();
+
+        private string basepath = AppDomain.CurrentDomain.BaseDirectory;
+        private string path;
 
         public Configuration()
         {
@@ -27,33 +26,11 @@ namespace BudgetControl.Migrations
 
         protected override void Seed(BudgetControl.DAL.BudgetContext context)
         {
-            string basepath = AppDomain.CurrentDomain.BaseDirectory;
-            string path;
 
 
             #region Account
 
-            //path = Path.Combine(basepath, @"Data\Account.txt");
-
-
-            //using (StreamReader reader = new StreamReader(path))
-            //{
-            //    using (TextFieldParser parser = new TextFieldParser(reader))
-            //    {
-            //        parser.TextFieldType = FieldType.Delimited;
-            //        parser.SetDelimiters("\t");
-            //        while (!parser.EndOfData)
-            //        {
-            //            string[] fields = parser.ReadFields();
-            //            accounts.Add(new Account
-            //            {
-            //                AccountID = fields[0],
-            //                AccountName = fields[1],
-            //                Status = RecordStatus.Active,
-            //            });
-            //        }
-            //    }
-            //}
+            //var accounts = ReadTextAccount();
             //accounts.ForEach(a => context.Accounts.AddOrUpdate(c => c.AccountID, a));
             //context.SaveChanges();
 
@@ -61,8 +38,78 @@ namespace BudgetControl.Migrations
             #endregion
 
             #region CostCenter
-            path = Path.Combine(basepath, @"Data\CostCenter.txt");
 
+            //var costcenters = ReadTextCostCenter();
+            //costcenters.ForEach(a => context.CostCenters.AddOrUpdate(c => c.CostCenterID, a));
+            //context.SaveChanges();
+
+            #endregion
+
+            #region PaymentCounter
+
+
+            //var paymentcounters = GeneratePaymentCounter();
+            //paymentcounters.ForEach(p => context.PaymentCounters.AddOrUpdate(a => a.CostCenterID, p));
+            //context.SaveChanges();
+
+            #endregion
+
+            #region Budget
+
+            //var budgets = ReadTextBudget();
+            //budgets.ForEach(s => context.Budgets.AddOrUpdate(c => new { c.AccountID, c.CostCenterID, c.Year }, s));
+            //context.SaveChanges();
+
+            #endregion
+
+            #region Employee
+
+            //var employees = ReadTextEmployee();
+            //employees.ForEach(e => context.Employees.AddOrUpdate(c => c.EmployeeID, e));
+            //context.SaveChanges();
+
+            #endregion
+            
+        }
+
+        #region Get Data from txt file
+
+        #region Account
+
+        private List<Account> ReadTextAccount()
+        {
+            List<Account> accounts = new List<Account>();
+            path = Path.Combine(basepath, @"Data\Account.txt");
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                using (TextFieldParser parser = new TextFieldParser(reader))
+                {
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters("\t");
+                    while (!parser.EndOfData)
+                    {
+                        string[] fields = parser.ReadFields();
+                        accounts.Add(new Account
+                        {
+                            AccountID = fields[0],
+                            AccountName = fields[1],
+                            Status = RecordStatus.Active,
+                        });
+                    }
+                }
+            }
+            return accounts;
+        }
+
+        #endregion
+
+        #region CostCenter
+
+        private List<CostCenter> ReadTextCostCenter()
+        {
+            List<CostCenter> costcenters = new List<CostCenter>();
+            path = Path.Combine(basepath, @"Data\CostCenter.txt");
 
             using (StreamReader reader = new StreamReader(path))
             {
@@ -74,88 +121,63 @@ namespace BudgetControl.Migrations
                     {
                         string[] fields = parser.ReadFields();
                         costcenters.Add(new CostCenter
-                            {
-                                CostCenterID = fields[0].Trim(),
-                                CostCenterName = fields[1],
-                                ShortName = fields[2],
-                                Status = RecordStatus.Active
-                            });
+                        {
+                            CostCenterID = fields[0].Trim(),
+                            CostCenterName = fields[1],
+                            ShortName = fields[2],
+                            Status = RecordStatus.Active
+                        });
                     }
                 }
             }
-            costcenters.ForEach(a => context.CostCenters.AddOrUpdate(c => c.CostCenterID, a));
-            context.SaveChanges();
-
-            #endregion
-
-            #region PaymentCounter
-
-            //List<PaymentCounter> paymentcounters = new List<PaymentCounter>();
-            
-            //foreach(var item in costcenters)
-            //{
-            //    string[] costcentersplit = item.ShortName.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
-
-            //    string shortcode;
-            //    if(costcentersplit[costcentersplit.Length - 1].Length < 3)
-            //    {
-            //        var l = costcentersplit.Length;
-            //        shortcode = costcentersplit[l - 2] + "." + costcentersplit[l - 1];
-            //    }
-            //    else
-            //    {
-            //        var test = costcentersplit[costcentersplit.Length - 1].Split(' ');
-            //        shortcode = test[test.Length - 1];
-            //    }
-                
-            //    paymentcounters.Add(new PaymentCounter()
-            //    {
-            //        CostCenterID = item.CostCenterID,
-            //        Year = "2560",
-            //        ShortCode = shortcode,
-            //        Number = 0
-            //    });
-            //}
-
-            //paymentcounters.ForEach(p => context.PaymentCounters.AddOrUpdate(a => a.CostCenterID, p));
-            //context.SaveChanges();
-
-            #endregion
-
-            #region Budget
-            //path = Path.Combine(basepath, @"Data\Budget.txt");
+            return costcenters;
+        }
 
 
-            //using (StreamReader reader = new StreamReader(path))
-            //{
-            //    using (TextFieldParser parser = new TextFieldParser(reader))
-            //    {
-            //        parser.TextFieldType = FieldType.Delimited;
-            //        parser.SetDelimiters("\t");
-            //        while (!parser.EndOfData)
-            //        {
-            //            string[] fields = parser.ReadFields();
-            //            budgets.Add(new Budget
-            //            {
-            //                BudgetID = Guid.NewGuid(),
-            //                AccountID = fields[0],
-            //                CostCenterID = fields[1],
-            //                Year = "2560",
-            //                BudgetAmount = float.Parse(fields[2]),
-            //                Status = BudgetStatus.Active
-            //            });
-            //        }
-            //    }
-            //}
+        #endregion
 
-            //budgets.ForEach(s => context.Budgets.AddOrUpdate(c => new { c.AccountID, c.CostCenterID, c.Year }, s));
-            //context.SaveChanges();
-            #endregion
+        #region PaymentCounter
 
-            #region Employee
+        public List<PaymentCounter> GeneratePaymentCounter()
+        {
+            List<PaymentCounter> paymentcounters = new List<PaymentCounter>();
+            var costcenters = ReadTextCostCenter();
+            foreach (var item in costcenters)
+            {
+                string[] costcentersplit = item.ShortName.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string shortcode;
+                if (costcentersplit[costcentersplit.Length - 1].Length < 3)
+                {
+                    var l = costcentersplit.Length;
+                    shortcode = costcentersplit[l - 2] + "." + costcentersplit[l - 1];
+                }
+                else
+                {
+                    var test = costcentersplit[costcentersplit.Length - 1].Split(' ');
+                    shortcode = test[test.Length - 1];
+                }
+
+                paymentcounters.Add(new PaymentCounter()
+                {
+                    CostCenterID = item.CostCenterID,
+                    Year = "2560",
+                    ShortCode = shortcode,
+                    Number = 0
+                });
+            }
+            return paymentcounters;
+        }
+
+        #endregion
+
+        #region Employee
+
+        private List<Employee> ReadTextEmployee()
+        {
+            List<Employee> employees = new List<Employee>();
             path = Path.Combine(basepath, @"Data\Employee_20171015.txt");
 
-            //List<Employee> employees = new List<Employee>();
             using (StreamReader reader = new StreamReader(path))
             {
                 using (TextFieldParser parser = new TextFieldParser(reader))
@@ -179,31 +201,51 @@ namespace BudgetControl.Migrations
                     }
                 }
             }
-            employees.ForEach(e => context.Employees.AddOrUpdate(c => c.EmployeeID, e));
-            context.SaveChanges();
-            #endregion
-
-            #region User
-
-            //employees.ForEach(e =>
-            //{
-            //    users.Add(new User
-            //    {
-            //        EmployeeID = e.EmployeeID,
-            //        UserName = e.EmployeeID,
-            //        Password = e.Password,
-            //        Role = UserRole.Normal,
-            //        Status = RecordStatus.Active
-            //    });
-            //});
-            
-            //users.ForEach(u => context.Users.AddOrUpdate(c => c.UserName, u));
-            //context.SaveChanges();
-
-            #endregion
-            
+            return employees;
         }
-        
+
+        #endregion
+
+        #region Budget
+
+        private List<Budget> ReadTextBudget()
+        {
+            List<Budget> budgets = new List<Budget>();
+            path = Path.Combine(basepath, @"Data\Budget.txt");
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                using (TextFieldParser parser = new TextFieldParser(reader))
+                {
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters("\t");
+                    while (!parser.EndOfData)
+                    {
+                        string[] fields = parser.ReadFields();
+                        budgets.Add(new Budget
+                        {
+                            BudgetID = Guid.NewGuid(),
+                            AccountID = fields[0],
+                            CostCenterID = fields[1],
+                            Year = "2560",
+                            BudgetAmount = decimal.Parse(fields[2]),
+                            Status = BudgetStatus.Active
+                        });
+                    }
+                }
+            }
+            return budgets;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region For migration : MoreEmployeeInfo
+
+
+
+        #endregion
 
     }
 }
