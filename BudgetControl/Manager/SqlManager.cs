c5dcbf71ec2488669eb1a6e8ca6d3e0f753065a2
504798 <panya.cs13@gmail.com>
2018-01-16 16:21:48 +0700

    Add SqlManager class

---
+++ BudgetControl/Manager/SqlManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace BudgetControl.Manager
+{
+    public static class SqlManager
+    {
+        public static string ConnectionString {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["BudgetContext"].ConnectionString;
+            }
+        }
+    }
+}
