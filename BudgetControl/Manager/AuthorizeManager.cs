04f5015505457e185ba023006e07a19dc99db5fe
504798 <panya.cs13@gmail.com>
2018-01-17 16:19:02 +0700

    Add authorize manager

---
+++ BudgetControl/Manager/AuthorizeManager.cs
@@ -0,0 +1,37 @@
+﻿using BudgetControl.DAL;
+using BudgetControl.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BudgetControl.Manager
+{
+    public class AuthorizeManager
+    {
+        private BudgetContext _db;
+
+        #region Constructor
+
+        public AuthorizeManager()
+        {
+            _db = new BudgetContext();
+        }
+
+        #endregion
+
+
+        #region Read
+
+        public List<AuthorizeCostCenter> GetByCostCenter(string costcentercode)
+        {
+            List<AuthorizeCostCenter> authorizes = new List<AuthorizeCostCenter>();
+
+            authorizes = _db.AuthorizeCostCenters.Where(a => a.CostCenterCode == costcentercode).ToList();
+
+            return authorizes;
+        }
+
+        #endregion  
+    }
+}
