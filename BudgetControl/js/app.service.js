40c7350e57df1c104e8c8596614ac4839dd5e964
504798 <panya.cs13@gmail.com>
2018-01-25 15:36:57 +0700

    Add more query string for return payment type

--- BudgetControl/js/app.service.js
+++ BudgetControl/js/app.service.js
@@ -24,7 +24,7 @@ budgetApp.factory('apiService', ['$resource', function ($resource) {
     }
 
     api.payment = function () {
-        return $resource('data/payment/:id?year=:year&costcenterid=:costcenterid', null, {
+        return $resource('data/payment/:id?year=:year&costcenterid=:costcenterid&type=:type', null, {
             'save': { method: 'POST' },
             'update': { method: 'PUT' },
             'remove': { method: 'DELETE'}
