53b7d006f552374385fdeb2c20f9da7570ed8c07
504798 <panya.cs13@gmail.com>
2018-01-25 14:06:56 +0700

    SqlDataReader method extension for get nullable datetime

---
+++ BudgetControl/Extensions/ReaderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BudgetControl.Extensions
+{
+    public static class ReaderExtensions
+    {
+        public static DateTime? GetNullableDateTime(this SqlDataReader reader, string name)
+        {
+            var col = reader.GetOrdinal(name);
+            return reader.IsDBNull(col) ?
+                        (DateTime?)null :
+                        (DateTime?)reader.GetDateTime(col);
+        }
+    }
+}
