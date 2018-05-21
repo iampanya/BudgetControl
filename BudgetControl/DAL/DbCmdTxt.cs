f951909a5cbfb287d9faba389494c12c43d76532
504798 <panya.cs13@gmail.com>
2018-05-21 11:19:20 +0700

    add class for sql command text

---
+++ BudgetControl/DAL/DbCmdTxt.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BudgetControl.DAL
+{
+    public static class DbCmdTxt
+    {
+        public static string cmd_upsert_budget_transaction
+        {
+            get
+            {
+                return @"
+UPDATE BudgetTransaction SET 
+	Amount = @Amount
+	, ModifiedBy = @ModifiedBy
+	, ModifiedAt = @ModifiedAt
+WHERE BudgetID = @BudgetID AND PaymentID = @PaymentID
+
+IF @@ROWCOUNT = 0
+	BEGIN
+		INSERT INTO BudgetTransaction
+			( 
+				BudgetTransactionID
+				, BudgetID
+				, PaymentID
+				, Description
+				, Amount
+				, PreviousAmount
+				, RemainAmount
+				, Type
+				, CreatedBy
+				, CreatedAt
+				, ModifiedBy
+				, ModifiedAt
+			)
+			VALUES
+			(
+				@Id
+				, @BudgetId
+				, @PaymentID
+				, @Description
+				, @Amount
+				, @PreviousAmount
+				, @RemainAmount
+				, @Type
+				, @CreatedBy
+				, @CreatedAt
+				, @ModifiedBy
+				, @ModifiedAt
+			)
+	END
+                ";
+            }
+        }
+    }
+}
