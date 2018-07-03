047401ef9d1a8c2c2304f83bc0fc1b067bfd75ea
504798 <panya.cs13@gmail.com>
2018-07-03 15:13:40 +0700

    add model current working cc

---
+++ BudgetControl/Models/CurrentWorkingCC.cs
@@ -0,0 +1,34 @@
+﻿using BudgetControl.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BudgetControl.Models
+{
+    public class CurrentWorkingCC : RecordTimeStamp
+    {
+        #region Constructor
+
+        public CurrentWorkingCC()
+        {
+
+        }
+
+        #endregion
+
+        #region Fields
+
+        public Guid Id { get; set; }
+
+        [StringLength(10)]
+        public string EmployeeID { get; set; }
+
+        [StringLength(10)]
+        public string WorkingCostCenterID { get; set; }
+
+        #endregion
+    }
+
+}
