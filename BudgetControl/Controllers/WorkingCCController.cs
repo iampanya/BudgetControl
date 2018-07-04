44200676a1cdd1be6520402b0ee5152b4edadb6a
504798 <panya.cs13@gmail.com>
2018-07-04 15:45:08 +0700

    get list working costcenter

--- BudgetControl/Controllers/WorkingCCController.cs
+++ BudgetControl/Controllers/WorkingCCController.cs
@@ -1,5 +1,6 @@
 ﻿using BudgetControl.DAL;
 using BudgetControl.Models;
+using BudgetControl.Sessions;
 using BudgetControl.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -19,7 +20,40 @@ namespace BudgetControl.Controllers
         {
             try
             {
-                returnobj.SetSuccess("");
+                var currentUser = AuthManager.GetCurrentUser();
+                var employeeid = currentUser.UserName;
+                var costcenterid = currentUser.Employee.CostCenterID;
+
+                using (var db = new BudgetContext())
+                {
+                    List<CostCenter> costcenterlist = new List<CostCenter>();
+                    costcenterlist.Add(db.CostCenters.Where(c => c.CostCenterID == costcenterid).FirstOrDefault());
+
+                    // Working CostCenter Condition
+                    var workingCondition = db.WorkingCCs.Where(e => e.EmployeeNo == employeeid || e.CostCenterCode == costcenterid).ToList();
+                    
+
+                    foreach (var item in workingCondition)
+                    {
+                        switch (item.Condition)
+                        {
+                            case ConditionType.ExactMatch:
+                                costcenterlist.AddRange(db.CostCenters.Where(c => c.CostCenterID == item.CCAStart).ToList());
+                                break;
+                            case ConditionType.Between:
+                                costcenterlist.AddRange(db.CostCenters.Where(c => c.CostCenterID.CompareTo(item.CCAStart) >= 0 && c.CostCenterID.CompareTo(item.CCAEnd) <= 0).ToList());
+                                break;
+                            case ConditionType.Contain:
+                                costcenterlist.AddRange(db.CostCenters.Where(c => c.CostCenterID.StartsWith(item.CCAStart)).ToList());
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    var finallist = costcenterlist.Select(c => new { c.CostCenterID, c.CostCenterName }).Distinct().OrderBy(c => c.CostCenterID).ToList();
+
+                    returnobj.SetSuccess(finallist);
+                }
             }
             catch (Exception ex)
             {
