7dd9b30013d306d6feb9ea27257aee820ab12c22
504798 <panya.cs13@gmail.com>
2018-09-12 15:05:11 +0700

    add payment transaction viewmodel

---
+++ BudgetControl/ViewModels/PaymentTransactionViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BudgetControl.ViewModels
+{
+    public class PaymentTransactionViewModel
+    {
+        #region Constructor
+
+        public PaymentTransactionViewModel()
+        {
+
+        }
+
+        #endregion
+
+        #region Fields
+
+        public Guid BudgetTransactionID { get; set; }
+        public string AccountID { get; set; }
+        public string AccountName { get; set; }
+        public string PaymentNo { get; set; }
+        public string Description { get; set; }
+        public string RequestBy { get; set; }
+        public string RequestByFullName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Amount { get; set; }
+
+        #endregion
+
+        #region Additional Fields
+
+        public string CreatedAtText
+        {
+            get
+            {
+                return CreatedAt.ToString("dd MMM yyyy");
+            }
+        }
+
+        #endregion
+
+    }
+}
